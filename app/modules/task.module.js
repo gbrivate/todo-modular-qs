7b6f9c6e02064483f34f6be719517fa0b01e53f4
Gabriel dos Santos <gabrielinflex@gmail.com>
2016-07-19 22:17:04 -0300

    remove files

--- app/modules/task.module.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var TaskModule = (function () {
-    function TaskModule() {
-    }
-    return TaskModule;
-}());
-exports.TaskModule = TaskModule;
-//# sourceMappingURL=task.module.js.map
